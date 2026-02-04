cebb5d83d5c2ff2ddda393fc0a3700e3d2063e46
sethuram333 <sethurammohan.m@gmail.com>
2026-02-04 14:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 function stringChop(str, size) {
-  // your code here
-	if(str.length===0){
-		return []
-	}
-	size =Number(size)
-	let result =[]
-	for(let i=0;i<str.length;i+=5){
-		result.push(str.slice(i,i+n))
-	}
-	return result
+  // handle null input
+  if (str === null) {
+    return [];
+  }
+
+  size = Number(size); // convert chunk size to number
+  let result = [];
+
+  for (let i = 0; i < str.length; i += size) {
+    let chunk = '';
+    for (let j = i; j < i + size && j < str.length; j++) {
+      chunk += str[j];
+    }
+    result.push(chunk);
+  }
+
+  return result;
 }
 
-// Do not change the code below
+// Do not change the code belo
 const str = prompt("Enter String");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
